b6a3f9f31a5f1ff63db1b76e44153433abb2a42f
linhanwei <2757104556@qq.com>
2016-10-09 15:32:45 +0800

    修改商城顶部分类链接

--- mall_m/templates/default/header.php
+++ mall_m/templates/default/header.php
@@ -1,6 +1,6 @@
 <?php defined('InSystem') or exit('Access Invalid!');?>
 <section class="search-box">
-    <a class="category-btn"> <img src="./templates/default/images/category-ico.png" /></a>
+    <a href="<?php BASE_SITE_URL;?>/mall_m/index.php?act=goods_class&op=index" class="category-btn"> <img src="./templates/default/images/category-ico.png" /></a>
     <div class="search-input">
         <input name="search-words" type="text" id="keyword" placeholder="搜索商品"> <a href="javascript:void(0);" class="search-btn"><img src="./templates/default/images/search-ico.png" /></a>
     </div>
